d51462220213e38e2f742f9b396ccde4ff9f55d5
Dan Gordon <gordond@byui.edu>
2023-08-15 12:02:56 -0600

    Reading a single file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,107 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BlazorGraph;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace BlazorComponentAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide the path to the .razor file as an argument.");
+                return;
+            }
+
+            var filePath = args[0];
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"File '{filePath}' does not exist.");
+                return;
+            }
+
+            var razorContent = File.ReadAllText(filePath);
+            var components = ExtractBlazorComponents(razorContent);
+
+            Console.WriteLine($"Found {components.Count} component(s) in {filePath}:");
+
+            foreach (var component in components)
+            {
+                Console.WriteLine($"- {component}");
+            }
+        }
+
+        public static List<string> ExtractBlazorComponents(string razorContent)
+        {
+            var csharpCode = GenerateCSharpFromRazor(razorContent);
+            var componentNames = ExtractComponentUsagesFromGeneratedCSharp(csharpCode);
+
+            return componentNames;
+        }
+
+        private static string GenerateCSharpFromRazor(string razorContent)
+        {
+            var engine = RazorProjectEngine.Create(RazorConfiguration.Default, RazorProjectFileSystem.Create("/"), (builder) => { });
+
+            var sourceDocument = RazorSourceDocument.Create(razorContent, "RazorFile");
+            var projectItem = new CustomRazorProjectItem(sourceDocument);
+            var codeDocument = engine.Process(projectItem);
+
+            var csharp = codeDocument.GetCSharpDocument().GeneratedCode;
+            return csharp;
+        }
+
+        private static List<string> ExtractComponentUsagesFromGeneratedCSharp(string csharpCode)
+        {
+            var tree = CSharpSyntaxTree.ParseText(csharpCode);
+            var root = tree.GetCompilationUnitRoot();
+
+            var componentNames = new HashSet<string>();
+
+            var invocations = root.DescendantNodes().OfType<InvocationExpressionSyntax>().ToList();
+            foreach (var invocation in invocations)
+            {
+                if (invocation.Expression is IdentifierNameSyntax methodName
+                    && methodName.Identifier.Text == "WriteLiteral")
+                {
+                    var argument = invocation.ArgumentList.Arguments.FirstOrDefault();
+                    if (argument != null)
+                    {
+                        var argValue = argument.Expression.NormalizeWhitespace().ToFullString().Trim('\"');
+                        componentNames.UnionWith(ExtractComponentNamesFromLiteral(argValue));
+                    }
+                }
+            }
+
+            return componentNames.ToList();
+        }
+
+        private static IEnumerable<string> ExtractComponentNamesFromLiteral(string literalValue)
+        {
+            var matches = Regex.Matches(literalValue, @"<(\w+)(\s|>)");
+            return matches.Cast<Match>().Select(match => match.Groups[1].Value).Where(name => !IsHtmlTag(name));
+        }
+
+        private static bool IsHtmlTag(string tagName)
+        {
+            // You can expand this list based on common HTML tags you encounter.
+            var htmlTags = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+    {
+        "a", "div", "span", "h1", "h2", "h3", "p", "br", "input", "button", "form",
+        "img", "ul", "li", "ol", "table", "thead", "tbody", "tr", "td", "th"
+    };
+
+            return htmlTags.Contains(tagName);
+        }
+
+
+    }
+}
---
+++ RazorProjectItem.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.Language;
+using System.Text;
+
+namespace BlazorGraph
+{
+    public class CustomRazorProjectItem : RazorProjectItem
+    {
+        private readonly RazorSourceDocument _sourceDocument;
+
+        public CustomRazorProjectItem(RazorSourceDocument sourceDocument)
+        {
+            _sourceDocument = sourceDocument;
+        }
+
+        public override string BasePath => "/";
+        public override string FilePath => _sourceDocument.FilePath;
+        public override string PhysicalPath => null;
+        public override bool Exists => true;
+
+        public override Stream Read()
+        {
+            var contentBuilder = new StringBuilder();
+            char[] buffer = new char[_sourceDocument.Length];
+            _sourceDocument.CopyTo(0, buffer, 0, buffer.Length);
+            contentBuilder.Append(buffer);
+            var contentBytes = Encoding.UTF8.GetBytes(contentBuilder.ToString());
+            return new MemoryStream(contentBytes);
+        }
+    }
+
+}
