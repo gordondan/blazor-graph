3d186f38096ec857aec4cc4556f3576fab28899b
Dan Gordon <gordond@byui.edu>
2023-08-16 16:15:18 -0600

    Pretty Cool!

--- VisioDiagramGenerator.cs
+++ VisioDiagramGenerator.cs
@@ -5,11 +5,10 @@ namespace BlazorGraph
 {
     public class VisioDiagramGenerator
     {
-        private const double init_y = 10;
+        private const double init_y = 6;
         private AppSettings _appSettings;
         private List<List<string>> _grid = new List<List<string>>();
-        private double currentX = .5;
-        private double currentY = 10;
+
         private HashSet<string> processedNodes = new HashSet<string>();
         private HashSet<string> stateNodes = new HashSet<string>();
         private double x_offset = 2.25;
@@ -30,9 +29,21 @@ namespace BlazorGraph
             application.Visible = true;
             var document = application.Documents.Add("");
             var page = application.ActivePage;
+            if (page.PageSheet.CellsU["PageWidth"].ResultIU < page.PageSheet.CellsU["PageHeight"].ResultIU)
+            {
+                double temp = page.PageSheet.CellsU["PageWidth"].ResultIU;
+                page.PageSheet.CellsU["PageWidth"].ResultIU = page.PageSheet.CellsU["PageHeight"].ResultIU;
+                page.PageSheet.CellsU["PageHeight"].ResultIU = temp;
+            }
 
             List<GraphNode> graphNodes = LoadGrid(componentRelations);
-            
+            var positionedNodes = GetPositionsForNodes(graphNodes);
+            var (maxX, maxY) = GetMaxCoordinates(positionedNodes);
+
+            EnsurePageSize(page, maxX, maxY);
+
+            // ... rest of the WriteToVisio method ...
+
             WriteToVisio(graphNodes, page);
 
             application.ActiveWindow.ViewFit = (short)VisWindowFit.visFitPage;
@@ -45,7 +56,59 @@ namespace BlazorGraph
             document.Close();
             application.Quit();
         }
+        private (double maxX, double maxY) GetMaxCoordinates(List<GraphNode> graphNodes)
+        {
+            double maxX = 0;
+            double maxY = 0;
 
+            foreach (var node in graphNodes)
+            {
+                if (node.X > maxX) maxX = node.X;
+                if (node.Y > maxY) maxY = node.Y;
+            }
+
+            return (maxX, maxY);
+        }
+
+        private void WriteToVisio(List<GraphNode> graphNodes, Page page)
+        {
+            var positionedNodes = GetPositionsForNodes(graphNodes);
+            ConsoleWriteGrid(positionedNodes);
+
+            Dictionary<int, List<Shape>> shapesByRow = new Dictionary<int, List<Shape>>();
+
+            foreach (var positionedNode in positionedNodes)
+            {
+                Console.WriteLine($"Processing: {positionedNode.ComponentName} at X: {positionedNode.X}, Y: {positionedNode.Y}");
+
+                Shape currentShape = CreateShape(page, positionedNode);
+
+                int row = (int)Math.Floor(positionedNode.Y);
+                if (!shapesByRow.ContainsKey(row))
+                    shapesByRow[row] = new List<Shape>();
+
+                shapesByRow[row].Add(currentShape);
+            }
+
+            foreach (var node in graphNodes)
+            {
+                Shape sourceShape = GetShapeByName(page, node.ComponentName);
+                foreach (var relatedNode in node.RelatedComponents)
+                {
+                    Shape targetShape = GetShapeByName(page, relatedNode.ComponentName);
+                    ConnectShapes(sourceShape, targetShape, page);
+                }
+            }
+
+            // Group shapes by row
+            foreach (var rowShapes in shapesByRow.Values)
+            {
+                GroupShapesByRow(rowShapes, page);
+            }
+
+            // Apply auto layout after grouping
+            //ApplyAutoLayout(page);
+        }
         private List<GraphNode> LoadGrid(Dictionary<string, List<string>> componentRelations)
         {
             List<GraphNode> graphNodes = new List<GraphNode>();
@@ -104,6 +167,10 @@ namespace BlazorGraph
                 Console.WriteLine($"Warning: Attempted to connect a null shape. Shape1: {shape1?.Text}, Shape2: {shape2?.Text}");
                 return;
             }
+            if(ComponentGraphProcessor.IsStateComponent(shape1.Text) || ComponentGraphProcessor.IsStateComponent(shape2.Text))
+            {
+                return;
+            }
             shape1.AutoConnect(shape2, VisAutoConnectDir.visAutoConnectDirNone);
         }
         private Shape CreateShape(Page page, GraphNode node)
@@ -113,8 +180,6 @@ namespace BlazorGraph
             var componentName = node.ComponentName;
 
             Console.WriteLine($"Creating shape for: {componentName} at X: {x}, Y: {y}");
-
-            EnsurePageSize(page);
 
             Shape shape = page.DrawRectangle(x,y, x + 2, y + 1);
             shape.Text = componentName;
@@ -156,24 +221,23 @@ namespace BlazorGraph
             return shape;
         }
 
-        private void EnsurePageSize(Page page)
+        private void EnsurePageSize(Page page, double maxX, double maxY)
         {
             const double margin = 1; // some space on all sides
 
             // Ensure height
-            if (currentY - margin < 0)
+            if (maxY + margin > page.PageSheet.CellsU["PageHeight"].ResultIU)
             {
-                double heightIncrease = Math.Abs(currentY) + margin;
-                page.PageSheet.CellsU["PageHeight"].ResultIU += heightIncrease;
-                currentY += heightIncrease;
+                page.PageSheet.CellsU["PageHeight"].ResultIU = maxY + margin;
             }
 
             // Ensure width
-            if (currentX + 2 + margin > page.PageSheet.CellsU["PageWidth"].ResultIU)
+            if (maxX + 2 + margin > page.PageSheet.CellsU["PageWidth"].ResultIU)
             {
-                page.PageSheet.CellsU["PageWidth"].ResultIU = currentX + margin;
+                page.PageSheet.CellsU["PageWidth"].ResultIU = maxX + 2 + margin;
             }
         }
+
 
         private Shape GetShapeByName(Page page, string name)
         {
@@ -190,37 +254,49 @@ namespace BlazorGraph
         {
             return graphNodes.Select(node => GetPositionForNode(node)).ToList();
         }
-
-        private void WriteToVisio(List<GraphNode> graphNodes, Page page)
+        private Shape GroupShapesByRow(List<Shape> shapesInRow, Page page)
         {
-            var positionedNodes = GetPositionsForNodes(graphNodes);
-            ConsoleWriteGrid(positionedNodes);  // This method will print grid for debugging
+            if (shapesInRow == null || !shapesInRow.Any()) return null;
 
-            // 1. Draw all the shapes first
-            foreach (var positionedNode in positionedNodes)
+            // Get the active window
+            Window window = page.Application.ActiveWindow;
+
+            // Clear current selection
+            window.DeselectAll();
+
+            // Add each shape in the list to the selection
+            foreach (Shape shape in shapesInRow)
             {
-                Console.WriteLine($"Processing: {positionedNode.ComponentName} at X: {positionedNode.X}, Y: {positionedNode.Y}");
-
-                EnsurePageSize(page);
-                CreateShape(page, positionedNode);
+                window.Select(shape,(short)VisSelectArgs.visSelect);
             }
 
-            // 2. Now make the connections between the shapes
-            foreach (var positionedNode in positionedNodes)
-            {
-                Shape currentShape = GetShapeByName(page, positionedNode.ComponentName);
-                foreach (var relatedNode in positionedNode.RelatedComponents)
-                {
-                    Shape relatedShape = GetShapeByName(page, relatedNode.ComponentName);
-                    if (currentShape != null && relatedShape != null) // Safety check
-                    {
-                        ConnectShapes(currentShape, relatedShape, page);
-                    }
-                }
-            }
+            // The active window's selection should now contain the shapes you added
+            Selection selection = window.Selection;
+
+            // Check if there's any selected shape before grouping
+            if (selection.Count == 0) return null;
+
+            // Group the selection
+            Shape groupShape = selection.Group();
+
+            return groupShape;
         }
 
 
+
+        //private void ApplyAutoLayout(Page page)
+        //{
+        //    // Layout solution might vary depending on the specific requirements
+        //    page.Layout(
+        //        Microsoft.Office.Interop.Visio.layout   VisLayoutStyles .visLORouteCenter,
+        //        Microsoft.Office.Interop.Visio.VisLayoutHorzAlignTypes.visLOHAlignCenter,
+        //        Microsoft.Office.Interop.Visio.VisLayoutLineAdjustFrom.visLOLineAdjustFromAll,
+        //        Microsoft.Office.Interop.Visio.VisLayoutLineAdjustTo.visLOLineAdjustToAll,
+        //        Microsoft.Office.Interop.Visio.VisLayoutLineRouteExt.visLORouteExtStraight,
+        //        0.125, 0.125, true
+        //    );
+
+        //}
         private GraphNode GetPositionForNode(GraphNode node)
         {
             int rowIndex = -1;
@@ -239,7 +315,7 @@ namespace BlazorGraph
             if (rowIndex != -1 && columnIndex != -1)
             {
                 double x = columnIndex * x_offset + 0.5;  // added offset
-                double y = init_y - rowIndex * y_offset;
+                double y = init_y + rowIndex * y_offset;
 
                 // Here, instead of modifying the existing node, we create a new one and return it.
                 GraphNode newNode = new GraphNode(node.ComponentName)
