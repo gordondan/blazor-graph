2eea1c891e36d6b8a3c846a49705ae59b8f2f458
Dan Gordon <gordond@byui.edu>
2023-08-16 11:20:52 -0600

    Removed duplicate nodes

--- VisioDiagramGenerator.cs
+++ VisioDiagramGenerator.cs
@@ -33,6 +33,10 @@ namespace BlazorGraph
                 ProcessComponent(component, page, componentRelations);
             }
 
+            application.ActiveWindow.ViewFit = (short)VisWindowFit.visFitPage;
+            application.ActiveWindow.Zoom = 1; // Sets zoom to 100%
+            application.ActiveWindow.SetViewRect(0, page.PageSheet.CellsU["PageHeight"].ResultIU,0,0);
+
             // Save the document to the specified file in AppSettings
             string currentDirectory = Directory.GetCurrentDirectory();
             string fullPath = System.IO.Path.Combine(currentDirectory, _appSettings.VisioFileName);
@@ -61,10 +65,11 @@ namespace BlazorGraph
             depth += 1; // Increase the depth for child components
 
             double originalX = currentX; // remember the starting X position
+            double originalY = currentY; // remember the starting Y position
 
             foreach (var relatedComponent in component.Value)
             {
-                currentY += y_offset; // Move down for each child
+                currentY += y_offset * depth; // Move down by 'depth' times the 'y_offset' for each child
 
                 // If the relatedComponent has its own children, process them
                 if (componentRelations.ContainsKey(relatedComponent))
@@ -73,9 +78,13 @@ namespace BlazorGraph
                     ProcessComponent(childComponentPair, page, componentRelations, depth);
                 }
                 else
-                {
-                    // Otherwise, just create a shape for the child
-                    Shape childShape = CreateShape(page, relatedComponent);
+                {// Inside the foreach loop in ProcessComponent
+                    Shape childShape = GetShapeByName(page, relatedComponent);
+                    if (childShape == null)
+                    {
+                        childShape = CreateShape(page, relatedComponent);
+                    }
+
                     ConnectShapes(parentShape, childShape, page);
                 }
 
@@ -83,6 +92,7 @@ namespace BlazorGraph
             }
 
             currentX = originalX; // reset the X position back to the parent's X position
+            currentY = originalY; // reset the Y position back to the parent's Y position
         }
 
         private void EnsurePageSize(Page page)
@@ -102,6 +112,17 @@ namespace BlazorGraph
             {
                 page.PageSheet.CellsU["PageWidth"].ResultIU = currentX + 2 + margin;
             }
+        }
+        private Shape GetShapeByName(Page page, string name)
+        {
+            foreach (Shape shape in page.Shapes)
+            {
+                if (shape.Text == name)
+                {
+                    return shape;
+                }
+            }
+            return null;
         }
 
         private Shape CreateShape(Page page, string componentName)
@@ -140,10 +161,10 @@ namespace BlazorGraph
 
         private void ConnectShapes(Shape shape1, Shape shape2, Page page)
         {
+
             shape1.AutoConnect(shape2, VisAutoConnectDir.visAutoConnectDirNone);
         }
-
-
+    
         private bool IsVendorComponent(string componentName)
         {
             return _appSettings.Vendors.Any(vendor => componentName.Contains(vendor));
