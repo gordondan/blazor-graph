b45061777cc513e1fc2cb4b490c9ed9d09db2eff
Dan Gordon <gordond@byui.edu>
2023-08-16 13:48:46 -0600

    Separating concerns

---
+++ ComponentGraphProcessor.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Office.Interop.Visio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorGraph
+{
+    public class ComponentGraphProcessor
+    {
+        private Dictionary<string, List<string>> componentRelations;
+        private HashSet<string> processedNodes = new HashSet<string>();
+        private HashSet<string> stateNodes = new HashSet<string>();
+        private List<List<string>> grid = new List<List<string>>();
+        private AppSettings _appSettings;
+
+        public ComponentGraphProcessor(Dictionary<string, List<string>> relations,AppSettings appSettings)
+        {
+            this.componentRelations = relations;
+            this._appSettings = appSettings;
+        }
+
+        // Move methods related to data processing like GetRootNodes, PreprocessRelationsBFS,
+        // IsRootNode, AddNodeToGrid, GetParent, DFS, and other related methods to this class.
+
+        public Dictionary<string, List<string>> Relations => componentRelations;
+        public HashSet<string> ProcessedNodes => processedNodes;
+        public List<List<string>> Grid => grid;
+
+
+
+        public HashSet<string> GetRootNodes(Dictionary<string, List<string>> componentRelations)
+        {
+            HashSet<string> allNodes = new HashSet<string>(componentRelations.Keys);
+            foreach (var relatedComponents in componentRelations.Values)
+            {
+                foreach (var relatedComponent in relatedComponents)
+                {
+                    allNodes.Remove(relatedComponent); // removes child nodes, leaving only the root nodes
+                }
+            }
+            return allNodes;
+        }
+
+        public bool IsRootNode(string nodeName, Dictionary<string, List<string>> componentRelations)
+        {
+            return !componentRelations.Values.Any(relList => relList.Contains(nodeName));
+        }
+
+        public bool IsVendorComponent(string componentName)
+        {
+            return _appSettings.Vendors.Any(vendor => componentName.Contains(vendor));
+        }
+
+        public void AddNodeToGrid(string nodeName, Dictionary<string, List<string>> componentRelations)
+        {
+            // Check if node is already in the grid
+            if (grid.Any(row => row.Contains(nodeName)))
+                return;
+
+            if (nodeName.EndsWith("State"))
+            {
+                if (grid.Count == 0)
+                    grid.Add(new List<string>());
+
+                grid[0].Add(nodeName);
+            }
+            else if (IsRootNode(nodeName, componentRelations))
+            {
+                while (grid.Count < 2)
+                    grid.Add(new List<string>());
+
+                grid[1].Add(nodeName);
+            }
+            else
+            {
+                var parent = GetParent(nodeName, componentRelations);
+                if (parent == null) return;
+
+                int parentRowIdx = grid.FindIndex(row => row.Contains(parent));
+                if (parentRowIdx == -1) return;
+
+                // Ensure next row exists
+                while (grid.Count <= parentRowIdx + 1)
+                    grid.Add(new List<string>());
+
+                grid[parentRowIdx + 1].Add(nodeName);
+            }
+        }
+
+        public List<string> PreprocessRelationsBFS(Dictionary<string, List<string>> componentRelations)
+        {
+            HashSet<string> visited = new HashSet<string>();
+            List<string> order = new List<string>();
+            Queue<string> queue = new Queue<string>();
+
+            // Add all root nodes to the queue to start BFS from them
+            foreach (var key in componentRelations.Keys)
+            {
+                if (IsRootNode(key, componentRelations))
+                {
+                    queue.Enqueue(key);
+                    visited.Add(key);
+                }
+            }
+
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                order.Add(currentNode);
+
+                if (componentRelations.ContainsKey(currentNode))
+                {
+                    foreach (var child in componentRelations[currentNode])
+                    {
+                        if (!visited.Contains(child))
+                        {
+                            visited.Add(child);
+                            queue.Enqueue(child);
+                        }
+                    }
+                }
+            }
+
+            return order;
+        }
+
+        private string GetParent(string childNode, Dictionary<string, List<string>> componentRelations)
+        {
+            foreach (var pair in componentRelations)
+            {
+                if (pair.Value.Contains(childNode))
+                {
+                    return pair.Key;  // The parent node
+                }
+            }
+            return null;  // No parent found
+        }
+
+        private void DFS(string node, Dictionary<string, List<string>> componentRelations, HashSet<string> visited, List<string> order)
+        {
+            if (visited.Contains(node))
+            {
+                return;
+            }
+
+            visited.Add(node);
+            order.Add(node);
+
+            if (componentRelations.ContainsKey(node))
+            {
+                foreach (var child in componentRelations[node])
+                {
+                    DFS(child, componentRelations, visited, order);
+                }
+            }
+        }
+    }
+
+}
--- VisioDiagramGenerator.cs
+++ VisioDiagramGenerator.cs
@@ -1,21 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Office.Interop.Visio;
 using System.Runtime.InteropServices;
-using Microsoft.Office.Interop.Visio;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace BlazorGraph
 {
     public class VisioDiagramGenerator
     {
+        private const double init_y = 10;
         private AppSettings _appSettings;
         private double currentX = .5;
         private double currentY = 10;
-        private const double init_y = 10;
-        private double y_offset = -1.25;
-        private double x_offset = 2.25;
-
+        private List<List<string>> grid = new List<List<string>>();
         private HashSet<string> processedNodes = new HashSet<string>();
         private HashSet<string> stateNodes = new HashSet<string>();
+        private double x_offset = 2.25;
+        private double y_offset = -1.25;
+        private ComponentGraphProcessor _graphProcessor;
 
         public VisioDiagramGenerator(AppSettings appSettings)
         {
@@ -25,21 +24,17 @@ namespace BlazorGraph
         public void GenerateVisioDiagram(Dictionary<string, List<string>> componentRelations)
         {
             var application = new Microsoft.Office.Interop.Visio.Application();
+            _graphProcessor = new ComponentGraphProcessor(componentRelations, _appSettings);
             application.Visible = true;
             var document = application.Documents.Add("");
             var page = application.ActivePage;
 
-            var rootNodes = GetRootNodes(componentRelations);
+            var rootNodes = _graphProcessor.GetRootNodes(componentRelations);
+            List<string> orderedNodes = _graphProcessor.PreprocessRelationsBFS(componentRelations);
 
-            foreach (var rootNode in rootNodes)
+            foreach (var rootNode in orderedNodes)
             {
-                ProcessComponent(new KeyValuePair<string, List<string>>(rootNode, componentRelations[rootNode]), page, componentRelations, isRoot: true);
-            }
-
-            foreach (var component in componentRelations)
-            {
-                if (!rootNodes.Contains(component.Key)) // This check ensures root nodes are not processed again.
-                    ProcessComponent(component, page, componentRelations);
+                ProcessComponent(rootNode, page, componentRelations, isRoot: true);
             }
 
             application.ActiveWindow.ViewFit = (short)VisWindowFit.visFitPage;
@@ -54,98 +49,109 @@ namespace BlazorGraph
             document.Close();
             application.Quit();
         }
-
-        private void ProcessComponent(KeyValuePair<string, List<string>> component, Page page, Dictionary<string, List<string>> componentRelations, int depth = 0, bool isRoot = false)
+        private void ProcessComponent(string rootNode, Page page, Dictionary<string, List<string>> componentRelations, bool isRoot)
         {
-            Console.WriteLine($"Processing: {component.Key} at X: {currentX}, Y: {currentY}");
+            Queue<string> nodesToProcess = new Queue<string>();
+            nodesToProcess.Enqueue(rootNode);
 
-            EnsurePageSize(page);
-
-            string nodeName = component.Key;
-
-            // Store original positions
-            double originalX = currentX;
-            double originalY = currentY;
-
-            // Check if the current node is a State node
-            if (nodeName.EndsWith("State") && !stateNodes.Contains(nodeName))
+            while (nodesToProcess.Count > 0)
             {
-                stateNodes.Add(nodeName);
-                currentY = init_y + (1.5 * y_offset);  // Two row heights above the root node row
+                string currentComponent = nodesToProcess.Dequeue();
 
-                if (stateNodes.Count > 1)
+                // Decide the position
+                _graphProcessor.AddNodeToGrid(currentComponent, componentRelations);
+
+                SetPositionFromGrid(currentComponent);
+
+                Console.WriteLine($"Processing: {currentComponent} at X: {currentX}, Y: {currentY}");
+
+                EnsurePageSize(page);
+
+                if (!processedNodes.Contains(currentComponent))
                 {
-                    currentX = x_offset * stateNodes.Count();  // Move to the right for subsequent state nodes
-                }
-                else
-                {
-                    currentX = x_offset;  // For the first state node, be one x_offset width in
-                }
-            }
-            if (processedNodes.Contains(nodeName))
-                return;  // skip nodes that have already been processed
+                    Shape currentShape = CreateShape(page, currentComponent);
+                    processedNodes.Add(currentComponent);
 
-            processedNodes.Add(nodeName);
-
-
-
-            Shape parentShape = CreateShape(page, nodeName);
-
-            foreach (var relatedComponent in component.Value)
-            {
-                if (isRoot) // For direct descendants of a root node
-                {
-                    currentY = originalY + y_offset;
-                }
-                else
-                {
-                    currentY += y_offset * depth; // For deeper descendants
-                }
-                // Check if the current node is a State node
-                if (relatedComponent.EndsWith("State") && !stateNodes.Contains(nodeName))
-                {
-                    stateNodes.Add(nodeName);
-                    currentY = init_y + (1.5 * y_offset);  // Two row heights above the root node row
-                    if (stateNodes.Count > 1)
+                    if (componentRelations.ContainsKey(currentComponent))
                     {
-                        currentX = x_offset * stateNodes.Count();  // Move to the right for subsequent state nodes
-                    }
-                    else
-                    {
-                        currentX = x_offset;  // For the first state node, be one x_offset width in
+                        foreach (string relatedComponent in componentRelations[currentComponent])
+                        {
+                            if (!processedNodes.Contains(relatedComponent))
+                            {
+                                nodesToProcess.Enqueue(relatedComponent);
+                            }
+
+                            // Connecting nodes
+                            Shape relatedShape = GetShapeByName(page, relatedComponent);
+                            if (relatedShape != null)
+                            {
+                                ConnectShapes(currentShape, relatedShape, page);
+                            }
+                        }
                     }
                 }
-                if (componentRelations.ContainsKey(relatedComponent))
-                {
-                    var childComponentPair = new KeyValuePair<string, List<string>>(relatedComponent, componentRelations[relatedComponent]);
-                    ProcessComponent(childComponentPair, page, componentRelations, depth + 1);
-                }
-                else
-                {
-                    Shape childShape = GetShapeByName(page, relatedComponent);
-                    if (childShape == null)
-                    {
-                        childShape = CreateShape(page, relatedComponent);
-                    }
-
-                    ConnectShapes(parentShape, childShape, page);
-                }
-
-                currentX += x_offset; // Move to the right for the next child (or sibling)
-            }
-
-            if (isRoot)
-            {
-                currentY = originalY; // Only reset the Y position for root nodes (their siblings should be on the same level)
-            }
-
-            if (stateNodes.Contains(nodeName))
-            {
-                currentX = originalX;
-                currentY = originalY;
             }
         }
 
+        private void SetPositionFromGrid(string nodeName)
+        {
+            int rowIndex = -1;
+            int columnIndex = -1;
+
+            for (int i = 0; i < grid.Count; i++)
+            {
+                columnIndex = grid[i].IndexOf(nodeName);
+                if (columnIndex != -1)
+                {
+                    rowIndex = i;
+                    break;
+                }
+            }
+
+            if (rowIndex != -1 && columnIndex != -1)
+            {
+                currentX = columnIndex * x_offset + 0.5;  // added offset
+                currentY = init_y - rowIndex * y_offset;
+            }
+        }
+        private void ConnectShapes(Shape shape1, Shape shape2, Page page)
+        {
+            shape1.AutoConnect(shape2, VisAutoConnectDir.visAutoConnectDirNone);
+        }
+
+        private Shape CreateShape(Page page, string componentName)
+        {
+            Console.WriteLine($"Creating shape for: {componentName} at X: {currentX}, Y: {currentY}");
+
+            EnsurePageSize(page);
+
+            Shape shape = page.DrawRectangle(currentX, currentY, currentX + 2, currentY + 1);
+            shape.Text = componentName;
+
+            // Setting shape rounding for rounded rectangle
+            shape.CellsU["Rounding"].ResultIU = 0.25;  // Adjust the value as necessary for the desired rounding
+
+            // Default to navy blue with white text
+            shape.CellsU["FillForegnd"].FormulaU = "RGB(0, 0, 128)";
+            shape.CellsU["Char.Color"].FormulaU = "RGB(255, 255, 255)";  // White color for text
+
+            // If it's a vendor component, set to lime green with white text
+            if (_graphProcessor.IsVendorComponent(componentName))
+            {
+                try
+                {
+                    shape.CellsU["FillForegnd"].FormulaU = "RGB(50, 205, 50)";  // Lime green
+                    shape.CellsU["Char.Color"].FormulaU = "RGB(255, 255, 255)";  // White color for text
+                }
+                catch (COMException ex)
+                {
+                    // Handle the error, perhaps logging it or notifying the user
+                    Console.WriteLine($"Error setting color for component {componentName}: {ex.Message}");
+                }
+            }
+
+            return shape;
+        }
         private void EnsurePageSize(Page page)
         {
             const double margin = 1; // some space on all sides
@@ -176,61 +182,27 @@ namespace BlazorGraph
             return null;
         }
 
-        private Shape CreateShape(Page page, string componentName)
+        private void UpdatePositionForRootNode(double originalY, bool isRoot)
         {
-            Console.WriteLine($"Creating shape for: {componentName} at X: {currentX}, Y: {currentY}");
-
-            EnsurePageSize(page);
-
-            Shape shape = page.DrawRectangle(currentX, currentY, currentX + 2, currentY + 1);
-            shape.Text = componentName;
-
-            // Setting shape rounding for rounded rectangle
-            shape.CellsU["Rounding"].ResultIU = 0.25;  // Adjust the value as necessary for the desired rounding
-
-            // Default to navy blue with white text
-            shape.CellsU["FillForegnd"].FormulaU = "RGB(0, 0, 128)";
-            shape.CellsU["Char.Color"].FormulaU = "RGB(255, 255, 255)";  // White color for text
-
-            // If it's a vendor component, set to lime green with white text
-            if (IsVendorComponent(componentName))
+            if (isRoot)
             {
-                try
-                {
-                    shape.CellsU["FillForegnd"].FormulaU = "RGB(50, 205, 50)";  // Lime green
-                    shape.CellsU["Char.Color"].FormulaU = "RGB(255, 255, 255)";  // White color for text
-                }
-                catch (COMException ex)
-                {
-                    // Handle the error, perhaps logging it or notifying the user
-                    Console.WriteLine($"Error setting color for component {componentName}: {ex.Message}");
-                }
+                currentY = originalY + y_offset;
             }
-
-            return shape;
-        }
-
-        private void ConnectShapes(Shape shape1, Shape shape2, Page page)
-        {
-
-            shape1.AutoConnect(shape2, VisAutoConnectDir.visAutoConnectDirNone);
-        }
-        private HashSet<string> GetRootNodes(Dictionary<string, List<string>> componentRelations)
-        {
-            HashSet<string> allNodes = new HashSet<string>(componentRelations.Keys);
-            foreach (var relatedComponents in componentRelations.Values)
+            else
             {
-                foreach (var relatedComponent in relatedComponents)
-                {
-                    allNodes.Remove(relatedComponent); // removes child nodes, leaving only the root nodes
-                }
+                currentY += y_offset;
             }
-            return allNodes;
+        }
+        private void UpdatePositionForStateNode(string nodeName)
+        {
+            if (!nodeName.EndsWith("State") || stateNodes.Contains(nodeName))
+                return;
+
+            stateNodes.Add(nodeName);
+            currentY = init_y - (1.5 * y_offset);  // Two row heights above the root node row
+            currentX = stateNodes.Count > 1 ? x_offset * stateNodes.Count() : x_offset;
         }
 
-        private bool IsVendorComponent(string componentName)
-        {
-            return _appSettings.Vendors.Any(vendor => componentName.Contains(vendor));
-        }
+
     }
 }
